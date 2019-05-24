2ef27bd69b9024fdd0f7ca4082d008c35c596358
amir mohammad davoodi <34346335+amirdavoodi98@users.noreply.github.com>
2019-05-24 12:07:57 -0700

    Part_4

---
+++ Purchasing_power.cs
@@ -0,0 +1,23 @@
+static List<string[,]> Purchasing_power(List<string[,]> matrix,int price)
+        {
+            List<string[,]> output = new List<string[,]>();
+            Dictionary<string[,], int> value_carpet = new Dictionary<string[,], int>();
+            Random rnd = new Random();
+            foreach (var item in matrix)
+            {
+                value_carpet.Add(item, rnd.Next(1, 20));
+            }
+            var items = from pair in value_carpet
+                        orderby pair.Value descending
+                        select pair;
+            foreach (var item in items)
+            {
+                if (price > item.Value)
+                {
+                    output.Add(item.Key);
+                    price -= item.Value;
+                } 
+            }
+
+            return output;
+        }
