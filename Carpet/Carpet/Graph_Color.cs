aa0a91de632e6fc861ea56b6b04e7ee26636c0a9
X.V <43823048+TerraDev@users.noreply.github.com>
2019-06-01 17:39:10 +0430

    Add files via upload

---
+++ Carpet/Carpet/Graph_Color.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carpet
+{
+    class Graph_Color
+    {
+        public int size { get; private set; }
+        public Graph_Color(int s)
+        {
+            size = s;
+        }
+
+        public bool[,] graph;
+
+        public Dictionary<int, int> Cols = new Dictionary<int, int>();
+
+        public void Calculate()
+        {
+            Cols[0] = 0;
+             SortedSet<int> tmp;
+
+            for(int i=1; i < size ; i++)
+            {
+                tmp = new SortedSet<int>();
+                for(int j=0; j < i ; j++)
+                {
+                   if(graph[i,j])
+                   {
+                        tmp.Add(Cols[j]);
+                   }
+                }
+                int u = 0;
+                foreach (int x in tmp)
+                {
+                    
+                    if(x != u)
+                    {
+                        break;
+                    }
+                    u++;
+                }
+                Cols[i] = u;
+            }
+        }
+
+        public int Show_max_Color()
+        {
+            SortedSet<int> a = new SortedSet<int>();
+            foreach(var item in Cols.Values)
+            {
+                a.Add(item);
+            }
+
+            return a.Last();
+        }
+    }
+}
---
+++ Carpet/Carpet/SecondWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace Carpet
+{
+    /// <summary>
+    /// Interaction logic for SecondWindow.xaml
+    /// </summary>
+    public partial class SecondWindow : Window
+    {
+        public SecondWindow()
+        {
+            InitializeComponent();
+        }
+
+        public bool clickD = true;
+        Graph_Color gc;
+
+        enum Click_U
+        {
+            off,
+            calculate,
+        }
+        Click_U State= Click_U.off;
+
+        private void Submit(object sender, RoutedEventArgs e)
+        {
+            if (clickD)
+            {
+                int a = Convert.ToInt32(Num_TextBox.Text);
+                Create_Matrix(a);
+                gc = new Graph_Color(a);
+            }
+            clickD = false;
+            State = Click_U.calculate;
+            
+        }
+
+        internal void Create_Matrix(int size)
+        {
+            for (int t = 0; t < size; t++)
+            {
+                RowDefinition RD = new RowDefinition();
+                RD.Height = new GridLength(30);
+                Matrix.RowDefinitions.Add(RD);
+                if (Matrix.RowDefinitions.Count() > 8)
+                {
+                    Matrix.Height += 30;
+                }
+            }
+
+            for (int t = 0; t < size; t++)
+            {
+                ColumnDefinition CD = new ColumnDefinition();
+                CD.Width = new GridLength(30);
+                Matrix.ColumnDefinitions.Add(CD);
+                if (Matrix.ColumnDefinitions.Count() > 29)
+                {
+                    Matrix.Width += 30;
+                }
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    TextBox Description = new TextBox();
+                    Description.Background = Brushes.Aqua;
+                    Description.Text = "0";
+                    Description.TextWrapping = TextWrapping.Wrap;
+                    Matrix.Children.Add(Description);
+                    Grid.SetRow(Description, i);
+                    Grid.SetColumn(Description, j);
+                }
+            }
+
+          //  Helper_Write("Insert values of SOURCE MATRIX" + '\n' + "Click Refresh button to restart");
+        }
+
+        private void Color_Click(object sender, RoutedEventArgs e)
+        {
+            int i = 0, j = 0;
+            int gsize = gc.size;
+            bool[,] mat = new bool[gsize, gsize];
+
+            foreach (TextBox box in Matrix.Children)
+            {
+                mat[i, j] = (box.Text == "1");
+                j++;
+                if (j >= gsize) { i++; j = 0; }
+            }
+            gc.graph = mat;
+
+            gc.Calculate();
+            Show_max.Text = Convert.ToString(gc.Show_max_Color() + 1);
+        }
+
+        private void Go_Next(object sender, RoutedEventArgs e)
+        {
+            //Process.Start();
+            this.Close();
+        }
+    }
+}
