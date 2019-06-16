d170849ad0b891cb7abe6092107659eca888f150
amir mohammad davoodi <34346335+amirdavoodi98@users.noreply.github.com>
2019-06-16 13:24:37 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.store_carpets = new System.Windows.Forms.Button();
+            this.sequence_aligment = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // store_carpets
+            // 
+            this.store_carpets.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.store_carpets.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.store_carpets.Location = new System.Drawing.Point(12, 12);
+            this.store_carpets.Name = "store_carpets";
+            this.store_carpets.Size = new System.Drawing.Size(106, 44);
+            this.store_carpets.TabIndex = 0;
+            this.store_carpets.Text = "store carpet pictures";
+            this.store_carpets.UseVisualStyleBackColor = true;
+            this.store_carpets.Click += new System.EventHandler(this.store_carpets_Click);
+            // 
+            // sequence_aligment
+            // 
+            this.sequence_aligment.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 8.25F);
+            this.sequence_aligment.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.sequence_aligment.Location = new System.Drawing.Point(635, 12);
+            this.sequence_aligment.Name = "sequence_aligment";
+            this.sequence_aligment.Size = new System.Drawing.Size(106, 44);
+            this.sequence_aligment.TabIndex = 1;
+            this.sequence_aligment.Text = "choose a picture to...";
+            this.sequence_aligment.UseVisualStyleBackColor = true;
+            this.sequence_aligment.Click += new System.EventHandler(this.sequence_aligment_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 76);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(230, 267);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(248, 76);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(256, 267);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(510, 76);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(231, 267);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 8.25F);
+            this.button1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(440, 455);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(635, 452);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(106, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(437, 296);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(524, 459);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "input the your money";
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(-3, 386);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(285, 86);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(753, 484);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.sequence_aligment);
+            this.Controls.Add(this.store_carpets);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button store_carpets;
+        private System.Windows.Forms.Button sequence_aligment;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        List<int> power_buy = new List<int>();
+        Dictionary<Bitmap, string> carpets_list = new Dictionary<Bitmap, string>();
+        Dictionary<Bitmap, int> value_carpet_list = new Dictionary<Bitmap, int>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void store_carpets_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            ofd.Filter = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg";
+            DialogResult dr = ofd.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                foreach (var file in ofd.FileNames)
+                {
+                    Bitmap tempbitmap = new Bitmap(file);
+                    string tempstr = "";
+                    for (int i = 0; i < 30; i++)
+                    {
+                        for (int j = 0; j < 40; j++)
+                        {
+                            Color color = tempbitmap.GetPixel(i, j);
+                            string a = System.Drawing.ColorTranslator.ToHtml(color);
+                            tempstr = tempstr.Insert(tempstr.Length,a.Remove(0, 1));
+                        }
+                    }
+                    carpets_list.Add(tempbitmap, tempstr);
+                    value_carpet_list.Add(tempbitmap, rnd.Next(1, 20));
+                }
+            }
+
+        }
+
+        private void sequence_aligment_Click(object sender, EventArgs e)
+        {
+            Dictionary<Bitmap, int> dic = new Dictionary<Bitmap, int>();
+            string pixel = "";
+            Bitmap bm;
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg";
+            DialogResult dr = ofd.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                bm = new Bitmap(ofd.FileName);
+                for (int i = 0; i < 30; i++)
+                {
+                    for (int j = 0; j < 40; j++)
+                    {
+                        Color color = bm.GetPixel(i, j);
+                        string a = System.Drawing.ColorTranslator.ToHtml(color);
+                        pixel = pixel.Insert(pixel.Length, a.Remove(0, 1));
+
+                    }
+                }
+            }
+            foreach (var item in carpets_list)
+            {
+                int i, j; // intialising variables  
+
+                int m = pixel.Length; // length of gene1  
+                int n = item.Value.Length; // length of gene2  
+
+                // table for storing optimal substructure answers  
+                int[,] dp = new int[n + m + 1, n + m + 1];
+                for (int q = 0; q < n + m + 1; q++)
+                    for (int w = 0; w < n + m + 1; w++)
+                        dp[q, w] = 0;
+
+                // intialising the table   
+                for (i = 0; i <= (n + m); i++)
+                {
+                    dp[i, 0] = i * 2;
+                    dp[0, i] = i * 2;
+                }
+
+                // calcuting the minimum penalty  
+                for (i = 1; i <= m; i++)
+                {
+                    for (j = 1; j <= n; j++)
+                    {
+                        if (pixel[i - 1] == item.Value[j - 1])
+                        {
+                            dp[i, j] = dp[i - 1, j - 1];
+                        }
+                        else
+                        {
+                            dp[i, j] = Math.Min(Math.Min(dp[i - 1, j - 1] + 1,
+                                            dp[i - 1, j] + 2),
+                                            dp[i, j - 1] + 2);
+                        }
+                    }
+                }
+                dic.Add(item.Key, dp[m, n]);
+            }
+            var items = from pair in dic
+                        orderby pair.Value ascending
+                        select pair;
+            int count = 0;
+            foreach (var item in items)
+            {
+                if (count == 0)
+                    pictureBox1.Image = item.Key;
+                else if (count == 1)
+                    pictureBox2.Image = item.Key;
+                else if (count == 2)
+                    pictureBox3.Image = item.Key;
+                else
+                    break;
+                count++;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int money = int.Parse(textBox1.Text);
+            int sum = 0;
+            var items = from pair in value_carpet_list
+                        orderby pair.Value ascending
+                        select pair;
+            foreach (var item in items)
+            {
+                if(sum + item.Value<=money)
+                {
+                    sum += item.Value;
+                    power_buy.Add(item.Value);
+                }
+            }
+
+            foreach (var item in power_buy)
+            {
+                listView1.Items.Add(item.ToString());
+            }
+        }
+        
+    }
+}
